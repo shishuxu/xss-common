e3f830fc460f0e916db4a1a9e8c42f88bae38308
xushishu <457683048@qq.com>
2022-04-27 21:06:39 +0800

    個人demo  公共依賴

---
+++ src/main/java/com/xss/xsscommon/XssCommonApplication.java
@@ -0,0 +1,13 @@
+package com.xss.xsscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XssCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XssCommonApplication.class, args);
+    }
+
+}
